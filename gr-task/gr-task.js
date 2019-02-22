fe8961ec47c4f82214197c600d2a31118a522ec5
Nasser Grainawi <nasser@codeaurora.org>
2019-02-22 11:13:40 -0700

    Simple Task UI
    Display a list of tasks in the 'FAIL', 'READY', or 'INVALID' states. No
    hierarchy between tasks and subtasks is displayed, just a flat list. Uses the
    task name or a hint value when available.
    
    The UI hides itself when there are no tasks to list.
    
    Change-Id: I64b2cd4ca944e89f5bb915caadf10576cc16550b

---
+++ gr-task/gr-task.js
@@ -0,0 +1,81 @@
+// Copyright (C) 2019 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+(function() {
+  'use strict';
+
+  Polymer({
+    is: 'gr-task',
+    properties: {
+      has_tasks: {
+        type: Boolean,
+        notify: true,
+        value: false,
+      }
+    },
+    attached() {
+       this._getTasks();
+       this._tasksFragment = document.createDocumentFragment();
+    },
+    _getTasks() {
+      const endpoint = `/changes/?q=change:${this.change._number}&--task--applicable`;
+
+      return this.plugin.restApi().get(endpoint).then(r => {
+          if (r && r.length === 1) {
+            let cinfo = r[0];
+            if (cinfo.plugins) {
+              cinfo.plugins.forEach(pinfo => {
+                if (pinfo.name === "task") {
+                  this.tasks = pinfo.roots;
+                  this._addTasks(this.tasks);
+                  if (this.has_tasks) {
+                    this.$.tasks.appendChild(this._tasksFragment);
+                  }
+                }
+              })
+            }
+          }
+      });
+    },
+    _computeMessage(task) {
+      switch (task.status) {
+        case 'FAIL':
+        case 'READY':
+        case 'INVALID':
+          return this._getTaskDescription(task);
+      }
+    },
+    _getTaskDescription(task) {
+      return task.hint || task.name;
+    },
+    _addTasks(tasks) {
+      if (!tasks) return;
+      tasks.forEach(task => {
+        let message = this._computeMessage(task);
+        if (message) {
+          this.has_tasks = true;
+          var taskMessage = document.createElement('div');
+          taskMessage.appendChild(document.createTextNode(message));
+          this._tasksFragment.appendChild(taskMessage);
+        }
+        this._addTasks(task.sub_tasks);
+      })
+    },
+    _hideIfNoTasks(has_tasks) {
+      if (!has_tasks) {
+        return "hide";
+      }
+      return "";
+    }
+  });
+})();
