bc413382a0796b4e169a313a72718bad7cffc8c2
Prudhvi Akhil Alahari <prudhvi.alahari@linaro.org>
2023-01-09 22:43:33 +0530

    task docker tests: add is_plugin_loaded test
    Add is_plugin_loaded test which fails the docker tests when task plugin
    does not load. This test prevents unnecessary execution of plugin tests.
    
    Change-Id: Idfc5b8c8e3420aa41da532120e4e90a0583ba987

--- test/docker/run_tests/start.sh
+++ test/docker/run_tests/start.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+die() { echo "ERROR: $1" >&2 ; exit 1 ; } # errormsg
+
+is_plugin_loaded() { # plugin_name
+    ssh -p 29418 "$GERRIT_HOST" gerrit plugin ls | awk '{print $1}' | grep -q "^$1\$"
+}
+
 USER_RUN_TESTS_DIR="$USER_HOME"/"$RUN_TESTS_DIR"
 mkdir "$USER_HOME"/task && cp -r /task/{src,test} "$USER_HOME"/task
 
@@ -15,6 +21,8 @@ echo "Creating a default user account ..."
 cat "$USER_HOME"/.ssh/id_rsa.pub | ssh -p 29418 -i /server-ssh-key/ssh_host_rsa_key \
   "Gerrit Code Review@$GERRIT_HOST" suexec --as "admin@example.com" -- gerrit create-account \
      --ssh-key - --email "gerrit_admin@localdomain"  --group "Administrators" "gerrit_admin"
+
+is_plugin_loaded "task" || die "Failed to load task plugin"
 
 ./"$USER_RUN_TESTS_DIR"/create-test-project-and-changes.sh
 ./"$USER_RUN_TESTS_DIR"/update-all-users-project.sh
