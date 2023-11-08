9c054270fd00ba620a0abdd6b1516f54d13bd088
Prudhvi Akhil Alahari <prudhvi.alahari@linaro.org>
2023-11-08 15:47:43 +0530

    Fix subtask-factory lookup to not fail when preloaded from another file
    Before this change, plugin failed to lookup a subtasks-factory when
    a task with this subtasks-factory was preloaded using a task
    reference [1]. While preloading a task, all the subtasks are copied
    to the current task. Since the subtasks-factory property was a String,
    it does not maintain the information regarding the location of the
    subtasks-factory. Thus, fix this issue by using ConfigSourcedValue as
    the type for subtasks-factory property, which helps in tracking the
    location of the subtasks-factory. Add tests for the same.
    
    [1]
    file: `All-Projects:refs/meta/config:task.config`
    ```
    [root "root task"]
        applicable = status:open
        preload-task = //common.config^simple task with subtasks
    ```
    
    file: `All-Projects:refs/meta/config:task/common.config`
    ```
    [task "simple task with subtasks"]
      applicable = is:open
      subtasks-factory = simple static tasks-factory
    
    [tasks-factory "simple static tasks-factory"]
        names-factory = names-factory static list
        pass = True
    
    [names-factory "names-factory static list"]
        type = static
        name = my a task
    ```
    
    Change-Id: Ie173ad1e29c511bb028a49d4c9b54108461bbe67

--- src/main/java/com/googlesource/gerrit/plugins/task/TaskTree.java
+++ src/main/java/com/googlesource/gerrit/plugins/task/TaskTree.java
@@ -94,6 +94,7 @@ public class TaskTree {
   protected final PredicateCache predicateCache;
   protected final MatchCache matchCache;
   protected final Preloader preloader;
+  protected final TaskConfigCache taskConfigCache;
   protected final TaskExpression.Factory taskExpressionFactory;
   protected final NodeList root = new NodeList();
   protected final Provider<ChangeQueryBuilder> changeQueryBuilderProvider;
@@ -127,6 +128,7 @@ public class TaskTree {
     this.changeQueryBuilderProvider = changeQueryBuilderProvider;
     this.predicateCache = predicateCache;
     this.matchCache = new MatchCache(predicateCache);
+    this.taskConfigCache = taskConfigCache;
     this.taskExpressionFactory = taskExpressionFactory;
     this.preloader = preloaderFactory.create(taskConfigCache);
   }
@@ -263,7 +265,7 @@ public class TaskTree {
       return String.valueOf(getChangeData().getId().get()) + TaskConfig.SEP + taskKey;
     }
 
-    public List<Node> getSubNodes() throws IOException, StorageException {
+    public List<Node> getSubNodes() throws IOException, StorageException, ConfigInvalidException {
       if (cachedNodes != null) {
         return refresh(cachedNodes);
       }
@@ -286,12 +288,14 @@ public class TaskTree {
       return nodes;
     }
 
-    public List<Node> getApplicableSubNodes() throws IOException, StorageException {
+    public List<Node> getApplicableSubNodes()
+        throws IOException, StorageException, ConfigInvalidException {
       return hasUnfilterableSubNodes ? getSubNodes() : new ApplicableNodeFilter().getSubNodes();
     }
 
     @Override
-    protected List<Node> loadSubNodes() throws IOException, StorageException {
+    protected List<Node> loadSubNodes()
+        throws IOException, StorageException, ConfigInvalidException {
       List<Task> cachedDefinitions = definitionsBySubSection.get(task.key().subSection());
       if (cachedDefinitions != null) {
         return new SubNodeFactory().createFromPreloaded(cachedDefinitions);
@@ -357,7 +361,7 @@ public class TaskTree {
       protected List<Node> nodes = new ArrayList<>();
       protected SubNodeFactory factory = new SubNodeFactory();
 
-      public List<Node> getSubNodes() throws IOException, StorageException {
+      public List<Node> getSubNodes() throws IOException, StorageException, ConfigInvalidException {
         addSubTasks();
         addSubTasksFactoryTasks();
         addSubTasksFiles();
@@ -407,11 +411,18 @@ public class TaskTree {
         }
       }
 
-      protected void addSubTasksFactoryTasks() throws IOException, StorageException {
-        for (String tasksFactoryName : task.subTasksFactories) {
-          TasksFactory tasksFactory = task.config.getTasksFactory(tasksFactoryName);
+      protected void addSubTasksFactoryTasks()
+          throws IOException, StorageException, ConfigInvalidException {
+        for (ConfigSourcedValue configSourcedValue : task.subTasksFactories) {
+          TasksFactory tasksFactory =
+              taskConfigCache
+                  .getTaskConfig(configSourcedValue.sourceFile())
+                  .getTasksFactory(configSourcedValue.value());
           if (tasksFactory != null) {
-            NamesFactory namesFactory = task.config.getNamesFactory(tasksFactory.namesFactory);
+            NamesFactory namesFactory =
+                taskConfigCache
+                    .getTaskConfig(configSourcedValue.sourceFile())
+                    .getNamesFactory(tasksFactory.namesFactory);
             if (namesFactory != null && namesFactory.type != null) {
               namesFactory = properties.getNamesFactory(namesFactory);
               switch (NamesFactoryType.getNamesFactoryType(namesFactory.type)) {
@@ -493,7 +504,7 @@ public class TaskTree {
 
       public ApplicableNodeFilter() throws StorageException {}
 
-      public List<Node> getSubNodes() throws IOException, StorageException {
+      public List<Node> getSubNodes() throws IOException, StorageException, ConfigInvalidException {
         if (nodesByBranch != null) {
           List<Node> nodes = nodesByBranch.get(branch);
           if (nodes != null) {
